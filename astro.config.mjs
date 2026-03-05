9cf24876f49e1872d5c0d5aaeadfdb005d93d134
Nicolas Sarkis <nicolas.sarkis@inria.fr>
2026-03-05 17:17:44 +0100

    last updated + gh-action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://nicolassarkis.github.io",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    ssr: {
+      optimizeDeps: {
+        include: ["dayjs"],
+      },
+    },
+    plugins: [tailwindcss()],
+  },
 });
